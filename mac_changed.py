7cca6d081892a0747b1aa4155fd11986a0845db8
AmrithaMS020 <168507757+AmrithaMS020@users.noreply.github.com>
2024-04-30 10:25:11 +0530

    Update mac_changed.py

--- mac_changed.py
+++ mac_changed.py
@@ -7,7 +7,7 @@ new_mac=input("new_mac >")
 
 subprocess.call(["ifconfig", interface,"down"])
 subprocess.call(["ifconfig", interface,"hw","ether",new_mac])
-subprocess.call(["ifconfig", xxinterface,"up"])
+subprocess.call(["ifconfig", interface,"up"])
 subprocess.call(["ifconfig"])
 
 
